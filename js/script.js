962c2d743a5f8934864fd6c12e7e7f7d38dcf7e6
Andrea030105 <tummoloandrea05@gmail.com>
2025-12-04 17:55:49 +0100

    bonus1-done

--- js/script.js
+++ js/script.js
@@ -1,27 +1,57 @@
-const imgArray = [
-    {
-        urlImg: "./img/01.webp",
-        titolo: "Spider-Man",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, fuga tenetur."
+const {
+    createApp
+} = Vue;
+
+createApp({
+    data() {
+        return {
+            imgArray: [
+                {
+                    urlImg: "./img/01.webp",
+                    titolo: "Spider-Man",
+                    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, fuga tenetur."
+                },
+                {
+                    urlImg: "./img/02.webp",
+                    titolo: "Ratchet & Clank",
+                    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, fuga tenetur."
+                },
+                {
+                    urlImg: "./img/03.webp",
+                    titolo: "Fortnite",
+                    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, fuga tenetur."
+                },
+                {
+                    urlImg: "./img/04.webp",
+                    titolo: "Stray",
+                    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, fuga tenetur."
+                },
+                {
+                    urlImg: "./img/05.webp",
+                    titolo: "Avengers",
+                    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, fuga tenetur."
+                },
+            ],
+            itemsActive: 0,
+        }
     },
-    {
-        urlImg: "./img/02.webp",
-        titolo: "Ratchet & Clank",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, fuga tenetur."
-    },
-    {
-        urlImg: "./img/03.webp",
-        titolo: "Fortnite",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, fuga tenetur."
-    },
-    {
-        urlImg: "./img/04.webp",
-        titolo: "Stray",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, fuga tenetur."
-    },
-    {
-        urlImg: "./img/05.webp",
-        titolo: "Avengers",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, fuga tenetur."
-    },
-]
+    methods: {
+        changeImg(index) {
+            this.itemsActive = index;
+        },
+
+        next() {
+            this.itemsActive++
+            if (this.itemsActive > this.imgArray.length - 1) {
+                this.itemsActive = 0
+            }
+        },
+
+        prev() {
+            this.itemsActive--
+            if (this.itemsActive < 0) {
+                this.itemsActive = this.imgArray.length - 1
+            }
+        }
+    }
+}).mount('#app')
