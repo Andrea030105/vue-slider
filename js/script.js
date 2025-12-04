109ec70a045b71646dcb3a6a2e0aeec6570fcdf6
Andrea030105 <tummoloandrea05@gmail.com>
2025-12-04 16:32:41 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const imgArray = [
+    {
+        urlImg: "./img/01.webp",
+        titolo: "Spider-Man",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, fuga tenetur."
+    },
+    {
+        urlImg: "./img/02.webp",
+        titolo: "Ratchet & Clank",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, fuga tenetur."
+    },
+    {
+        urlImg: "./img/03.webp",
+        titolo: "Fortnite",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, fuga tenetur."
+    },
+    {
+        urlImg: "./img/04.webp",
+        titolo: "Stray",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, fuga tenetur."
+    },
+    {
+        urlImg: "./img/05.webp",
+        titolo: "Avengers",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, fuga tenetur."
+    },
+]
---
+++ readme.txt
@@ -0,0 +1,12 @@
+TRACE
+
+Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue.
+
+BONUS 1:
+Al click su una thumb, visualizzare in grande l'immagine corrispondente.
+
+BONUS 2:
+Applicare l'autoplya allo slider: ohni 3 secondi cambia immagine automaticamente.
+
+BONUS 3:
+Quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce. 
